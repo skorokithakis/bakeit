19a41c69cc07137d61285dfe533f1d100de7e193
Stavros Korokithakis <hi@stavros.io>
2025-08-25 16:01:46 +0300

    ci: Add Release Please

--- bakeit/__init__.py
+++ bakeit/__init__.py
@@ -1,4 +1,11 @@
 # flake8: noqa
 
-from bakeit.version import __version__
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("bakeit")
+except PackageNotFoundError:
+    # Package is not installed.
+    __version__ = "0.0.0+unknown"
+
 from bakeit.uploader import PasteryUploader
